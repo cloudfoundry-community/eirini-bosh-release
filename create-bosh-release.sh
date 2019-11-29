4ba9381d0b4eaeac524720ea2f57361279a89edb
André Duffeck <aduffeck@suse.com>
2019-11-29 09:20:15 +0100

    Fix passing on create-release args
    The old code wasn't working with
    
    ./create-bosh-release.sh --final --tarball=foo.tgz
    
    for example.

--- create-bosh-release.sh
+++ create-bosh-release.sh
@@ -22,4 +22,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 set -x
 
-docker run --mount type=bind,source=$DIR,target=/bosh-release relintdockerhubpushbot/cf-deployment-concourse-tasks /bin/bash -c "cd bosh-release && bosh create-release $@"
+cmd="cd bosh-release && bosh create-release $@"
+docker run --mount type=bind,source=${DIR},target=/bosh-release relintdockerhubpushbot/cf-deployment-concourse-tasks /bin/bash -c "${cmd}"
